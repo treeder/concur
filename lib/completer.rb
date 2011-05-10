a0a2f986e3d9afd8687be24b1866b1ac3228dbf2
Travis Reeder <travis@appoxy.com>
2011-05-09 23:19:55 -0700

    Added stub for Completer class

---
+++ lib/completer.rb
@@ -0,0 +1,12 @@
+# Decouples execution of tasks from getting completed tasks so you can add tasks for execution to a Completer
+# and retrieve completed tasks (first completed, first out) from it.
+#
+# based on http://download.oracle.com/javase/6/docs/api/java/util/concurrent/CompletionService.html
+
+module Concur
+
+  class Completer
+    
+  end
+
+end
